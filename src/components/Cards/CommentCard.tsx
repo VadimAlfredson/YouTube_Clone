882fe3e802fb0c3604a36e8334c4ab60e722a0bb
VadimAlfredson <damonkvantel@gmail.com>
2023-04-06 21:30:05 +0500

    comment mui

--- src/components/Cards/CommentCard.tsx
+++ src/components/Cards/CommentCard.tsx
@@ -1,24 +1,50 @@
 import React from "react";
-import {Box, Card, CardMedia, Stack, Typography} from "@mui/material";
+import {
+    Avatar,
+    Box,
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    CardMedia, Grid,
+    IconButton,
+    Stack,
+    Typography
+} from "@mui/material";
 import {commentType} from "../../types/typesItems";
 
 
-const CommentCard:React.FC<{ comment: commentType }> = ({comment}) => {
+const CommentCard: React.FC<{ comment: commentType }> = ({comment}) => {
     return (
-        <Stack direction='row' sx={{backgroundColor: '#1e1e1e', margin: 2, borderRadius: '3px'}}>
-            <Box flex={1} sx={{width: '100px', borderRadius: '50%'}}>
-                <div style={{width: '100px', height: '100px', backgroundColor: 'red', borderRadius: '50%'} }>
-                <img src={comment.snippet.topLevelComment.snippet.authorProfileImageUrl}
-                style={{width: '100px'}}
-                /></div>
-            </Box>
-            <Box display='flex' flexDirection='column' justifyContent='flex-start'>
-                <Typography color='white'>{comment.snippet.topLevelComment.snippet.authorDisplayName}</Typography>
-                <Typography color='white'>{comment.snippet.topLevelComment.snippet.textDisplay}</Typography>
-                <Typography color='white'>{comment.snippet.topLevelComment.snippet.likeCount}</Typography>
 
-            </Box>
-        </Stack>
+
+        <Card sx={{backgroundColor: '#1e1e1e', margin: 1, borderRadius: '15px', color: 'white'}}>
+            <CardHeader sx={{m: '0'}}
+                        avatar={<Avatar sx={{backgroundColor: "grey"}}
+                                        alt={'V'}
+                                        src={comment.snippet.topLevelComment.snippet.authorProfileImageUrl}
+                        />}
+                        title={
+                            <Typography
+                                sx={{color: 'white'}}>{comment.snippet.topLevelComment.snippet.authorDisplayName}</Typography>
+                        }
+                        subheader={<Typography
+                            sx={{color: 'grey'}}>{comment.snippet.topLevelComment.snippet.publishedAt}</Typography>}
+            />
+            <CardContent sx={{m: '0', pt: '3px', pb: '3px'}}>
+                <Typography color='white'>{comment.snippet.topLevelComment.snippet.textDisplay}</Typography>
+            </CardContent>
+            <CardActions disableSpacing sx={{
+                m: '0', pt: '3px',
+                pb: '3px', mr: '10%', justifyContent: 'right'
+            }}>
+                <IconButton aria-label="add to favorites" sx={{}}>
+                    {`<З`}
+                </IconButton>
+                <Typography color='white'>{comment.snippet.topLevelComment.snippet.likeCount}</Typography>
+            </CardActions>
+        </Card>
+
     )
 }
 
