6e901a7234218bd5af4e68f58a9261c553ff3c3b
VadimAlfredson <damonkvantel@gmail.com>
2023-04-09 17:17:27 +0500

    app route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import {Box} from "@mui/material";
 import Navbar from "./components/Navbar/Navbar";
 import Feed from "./components/Feed/Feed";
@@ -8,7 +8,7 @@ import ChannelDetail from "./components/ChannelDetail/ChannelDetail";
 import SearchFeed from "./components/SearchFeed/SearchFeed";
 
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Box sx={{backgroundColor: '#000'}}>
           <Navbar />
           <Routes>
@@ -16,6 +16,7 @@ const App = () => (
               <Route path={'/video/:id'} element={<VideoDetail />}/>
               <Route path={'/channel/:id'} element={<ChannelDetail />}/>
               <Route path={'/search/:searchTerm'} element={<SearchFeed />}/>
+              <Route path={'/*'} element={<Navigate to={'/'} />} />
           </Routes>
       </Box>
   </BrowserRouter>
