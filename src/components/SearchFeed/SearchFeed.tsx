950af90799b3bf1c4c604d5f5278f01d088272d2
VadimAlfredson <damonkvantel@gmail.com>
2023-04-10 23:34:09 +0500

    search error

--- src/components/SearchFeed/SearchFeed.tsx
+++ src/components/SearchFeed/SearchFeed.tsx
@@ -2,50 +2,52 @@ import React, {useEffect, useState} from "react";
 import {Box, Typography} from "@mui/material";
 import {videosItemType} from "../../types/typesItems";
 import {useAppDispatch, useAppSelector} from "../../types/hooks";
-import { getSuggestedVideos } from "../../redux/suggestedVideos_reducer";
+import {getSuggestedVideos} from "../../redux/suggestedVideos_reducer";
 import Videos from "../Feed/Videos/Videos";
-import {useParams} from "react-router-dom";
-
+import {useNavigate, useParams} from "react-router-dom";
 
 
 const SearchFeed = () => {
- const dispatch = useAppDispatch()
- const videosItems = useAppSelector(state => state.videos.videos)
-    const { searchTerm } = useParams();
+    const dispatch = useAppDispatch()
+    const videosItems = useAppSelector(state => state.videos.videos)
+    const {searchTerm} = useParams();
+    const navigate = useNavigate()
 
- const [videos, setVideos] = useState(videosItems as videosItemType[])
+    const [videos, setVideos] = useState(videosItems as videosItemType[])
 
- useEffect(() => {
-  if (videos !== videosItems) {
-   setVideos(videosItems)
-  }
- }, [videosItems])
- useEffect(() => {
-     if (searchTerm !== ''){
-  dispatch(getSuggestedVideos(`${searchTerm}`))}
- }, [searchTerm])
+    useEffect(() => {
+        if (videos !== videosItems) {
+            setVideos(videosItems)
+        }
+    }, [videosItems])
+    useEffect(() => {
+        if (searchTerm !== '') {
+            dispatch(getSuggestedVideos(`${searchTerm}`))
+            navigate(`/search/${searchTerm}`)
+        }
+    }, [searchTerm])
 
 
- return (
-      <Box
-          p={2}
-          sx={{
-           overflowY: 'auto',
-           height: '90vh',
-           flex: 2
-          }}>
-       <Typography
-           variant="h4"
-           mb={2}
-           sx={{color: 'white'}}
-       >
-        Search result for <span
-           style={{color: '#F31503'}}
-       >{searchTerm}</span>
-       </Typography>
-       <Videos videos={videos}/>
-      </Box>
- )
+    return (
+        <Box
+            p={2}
+            sx={{
+                overflowY: 'auto',
+                height: '90vh',
+                flex: 2
+            }}>
+            <Typography
+                variant="h4"
+                mb={2}
+                sx={{color: 'white'}}
+            >
+                Search result for <span
+                style={{color: '#F31503'}}
+            >{searchTerm}</span>
+            </Typography>
+            <Videos videos={videos}/>
+        </Box>
+    )
 }
 
 export default SearchFeed
